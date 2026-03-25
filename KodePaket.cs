9ff0cd547bd64e395d21ba795f5f97243ef4ba97
rayen <rapriadi706@gmail.com>
2026-03-25 07:19:24 +0700

    second commit

---
+++ KodePaket.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modul4_103022400071
+{
+    class KodePaket
+    {
+        private Dictionary<string, string> paket = new Dictionary<string, string>()
+        {
+            {"Basic", "P201"},
+            {"Standard", "P202"},
+            {"Premium", "P203"},
+            {"Unlimited","P204"},
+            {"Gaming","P205"},
+            {"Streaming","P206"},
+            {"Family", "P207"},
+            {"Business", "P208"},
+            {"Student","P209" },
+            {"Traveler", "P210"}
+        };
+
+        public string getKodePaket(string namaPaket)
+        {
+            if (paket.ContainsKey(namaPaket))
+            {
+                return paket[namaPaket];
+            }
+            else
+            {
+                return "Kode paket tidak ditemukan";
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Modul4_103022400071;
+
+KodePaket kp  = new KodePaket();
+
+Console.WriteLine("Masukkan nama paket (Basic, Standard, Premium, Unlimited, Gaming, Streaming, Family, Business, Student, Traveler): ");
+string namaPaket = Console.ReadLine();
+
+Console.WriteLine("Kode paket:" + kp.getKodePaket(namaPaket));
+Console.ReadLine();
